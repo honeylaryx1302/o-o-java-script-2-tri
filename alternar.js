0711776a80efaba2c603283cc7809d2fb29d407c
honeylaryx1302 <162998971+honeylaryx1302@users.noreply.github.com>
2024-05-29 08:34:40 -0300

    Add files via upload

---
+++ alternar.js
@@ -0,0 +1,15 @@
+const pessoa ={
+    nome:"luma",
+    profissao:"engenharia",
+};
+
+console.log(pessoa.telefone);
+
+pessoa.telefone = "41 9999-9999";
+
+console.log(pessoa.telefone);
+
+pessoa.nome = "Luma Silva";
+
+console.log(pessoa)
+
---
+++ tipo.js
@@ -0,0 +1,9 @@
+const cliente = {
+    nome:"João",
+    idade:24,
+    email:"joão@hotmail.com",
+    telefone:["(41) 9999-0000","(41) 9988-0000"],
+};
+
+console.log(cliente)
+console.log(cliente.telefone)
