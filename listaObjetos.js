d31a7dfa983287199fb655b979431e1e4f852a28
honeylaryx1302 <162998971+honeylaryx1302@users.noreply.github.com>
2024-06-20 08:36:21 -0300

    Add files via upload

---
+++ listaObjetos.js
@@ -0,0 +1,17 @@
+const cliente = {
+    nome:"João",
+    idade:24,
+    email:"joão@hotmail.com",
+    telefone:["(41) 9999-0000","(41) 9988-0000"],
+};
+
+cliente.endereco = [
+ {
+    rua : "R. Joseph Climber",
+    numero : 1337,
+    apartamento : true,
+    complemento : "Ap 934",
+},
+];
+
+console.log(cliente.endereco);
